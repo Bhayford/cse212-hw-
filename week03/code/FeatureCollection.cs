99ed5c578f2ed31956c60a11fdb51f1c7347aa92
bhayford <benjaminbrandford00@gmail.com>
2024-10-04 02:45:25 +0000

    w03

--- week03/code/FeatureCollection.cs
+++ week03/code/FeatureCollection.cs
@@ -1,5 +1,27 @@
-public class FeatureCollection
+using System.Collections.Generic;
+
+public class FeatureCollections
 {
-    // TODO Problem 5 - ADD YOUR CODE HERE
-    // Create additional classes as necessary
+    public string Type { get; set; } // Type of the collection, usually "FeatureCollection"
+    public List<Feature> Features { get; set; } // List of features (earthquakes)
+}
+
+public class Features
+{
+    public string Type { get; set; } // Type of the feature, usually "Feature"
+    public Properties Properties { get; set; } // Properties containing the earthquake info
+    public Geometry Geometry { get; set; } // Geometry information (location)
+    public string Id { get; set; } // Unique identifier for the feature
+}
+
+public class Propertiess
+{
+    public double Mag { get; set; } // Magnitude of the earthquake
+    public string Place { get; set; } // Location of the earthquake
+}
+
+public class Geometry
+{
+    public string Type { get; set; } // Type of geometry, typically "Point"
+    public List<double> Coordinates { get; set; } // Coordinates [longitude, latitude]
 }
